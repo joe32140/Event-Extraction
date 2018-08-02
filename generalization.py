ec3d9c42ab94714708bf659128d2a74ce2d02a6c
cloud60138 <cloud60138@camel.iis.sinica.edu.tw>
2018-08-02 17:36:17 +0800

    [U]

--- generalization.py
+++ generalization.py
@@ -12,7 +12,7 @@ class Generalization():
     def run(self, events):
         output=[]
         for e in events:
-            print(e)
+            #print(e)
             tmp=[]
             #print("============S=============")
             tmp.append(self.get_hypernym(e[0]))
@@ -57,7 +57,7 @@ class Generalization():
             else:
                 print(hypernym)
                 break
-        print("Hi",word.name())
+        #print("Hi",word.name())
         return word.name()
 
     def replace_verb(self, word):
@@ -117,7 +117,7 @@ class DependencyParser():
                     'O':('<empty>','<empty>'), 'M':('<empty>','<empty>')}
             for triple in sent[0]:
                 t1, t2, t3 = triple[0], triple[1], triple[2]
-                if t2=='nsubj' and t1[1][0]=='V':
+                if t2[0:5]=='nsubj' and t1[1][0]=='V':
                     if tmp['V'][0]=='<empty>': tmp['V']=t1
                     if tmp['S'][0]=='<empty>': tmp['S']=t3
                 elif t2=='nsubj' and t1[1][0] in 'VJNP':
@@ -131,6 +131,13 @@ class DependencyParser():
                     if tmp['O'][0]=='<empty>': tmp['O']=t3
                 elif t2=='ccomp' or t2=='iobj' or t2=='pobj' or t2=='xcomp':
                     if tmp['M'][0]=='<empty>': tmp['M']=t3
+                elif t2 == 'auxpass':
+                    if tmp['V'][0]=='<empty>': tmp['V']=t1
+                elif t2[0:3] == 'acl':
+                    if tmp['S'][0]=='<empty>': tmp['S']=t1
+                elif t2[0:4] == 'nmod':
+                    if tmp['V'][0]=='<empty>' and t1[1][0] =='V': tmp['V']=t1
+                    if tmp['O'][0]=='<empty>': tmp['O']=t3
                     continue
                 else:
                     continue
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from generalization import DependencyParser, Generalization
 import json
 import re
+from random import randrange
 
 if __name__ == '__main__':
     parser = DependencyParser()
@@ -14,17 +15,21 @@ if __name__ == '__main__':
         #n_batch=40
         for i in range(n_batch+1):
             print(f"Batch {i}===============================")
+
             start = i*batch_size
             end = (i+1)*batch_size if (i+1)*batch_size<len(data['annotations']) else len(data['annotations'])
             tmp=[]
+            random_index = randrange(0,(end-start))
             for j in range(start,end):
                 tmp.append(re.sub("\[|\]","",data['annotations'][j][0]['text']))
-            #print(tmp)
+            print("tmp:", tmp[random_index])
             parsed_sents, aa = parser.get_SVOM(tmp)
             g_sents = G.run(parsed_sents)
-            #print(aa)
-            #print(g_sents)
+            print("parsed_sents:",parsed_sents[random_index])
+            print("aa:",aa[random_index])
+            print("g_sents:", g_sents[random_index])
+            print(len(tmp),len(parsed_sents),len(aa), len(g_sents))
             #if i == 50:exit()
             for j in range(start, end):
                 data['annotations'][j][0]['event']=g_sents[j%batch_size]
-        json.dump(data, open(f'/home/joe32140/event-visual-storytelling/data/event_clean_{file_path}.json', 'w'), indent=4)
+        json.dump(data, open(f'/home/cloud60138/Event-Extraction/data/event_clean_{file_path}.json', 'w'), indent=4)
